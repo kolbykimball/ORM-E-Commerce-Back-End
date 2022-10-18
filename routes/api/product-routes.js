4143292dc467df06604c77b5e8867373e3ec357f
kolbykimball <kolby.kimball10@gmail.com>
2022-10-17 18:28:38 -0600

    fixed await async issue

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
@@ -16,12 +16,12 @@ router.get('/', (req, res) => {
     });
     res.status(200).json(productData);
   } catch (err) {
-    res.status(500).json(err):
+    res.status(500).json(err);
   }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
@@ -117,7 +117,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
